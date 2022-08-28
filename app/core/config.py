341fcfb7f0a02f4f9eefa775a844d74c336c48f1
V4SRandiSPB <ivan@v4scale.com>
2022-08-28 14:52:00 +0300

    feat: add user models + compose for DB creating

--- app/core/config.py
+++ app/core/config.py
@@ -5,6 +5,7 @@ class Setting(BaseConfig):
     DEFAULT_HOST: str = "localhost"
     DEFAULT_PORT: int = 8001
     SERVICE_NAME: str = "Event manager"
+    DATABASE_URL: str = "localhost:5432/postgres"
     DEBUG: bool = True
 
 
---
+++ app/models/users.py
@@ -0,0 +1,24 @@
+from sqlalchemy import MetaData, Table, Integer, String, Column, Enum
+import enum
+
+
+class UserRole(enum.Enum):
+    common = 1
+    admin = 2
+    supervisor = 3
+
+
+metadata = MetaData()
+
+
+users_table = Table(
+    "users",
+    metadata,
+    Column("id", Integer, primary_key=True, index=True),
+    Column("name", String(100), nullable=False),
+    Column("surname", String(100), nullable=False),
+    Column("email", String(50), unique=True, index=True),
+    Column("vk_link", String(150), unique=True),
+    Column("password", String()),
+    Column("role", Enum(UserRole))
+)
